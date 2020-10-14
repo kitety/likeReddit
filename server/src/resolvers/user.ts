c36e507b8baa20e9ca897a923be0952084a4dade
Kitety <aizaizuori@gmail.com>
2020-10-14 06:11:16 +0000

    feat: 路径引用完善

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { User } from "src/entities/User";
+import { User } from "../entities/User";
 import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
 import argon2 from "argon2";
